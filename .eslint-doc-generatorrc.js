bc96b6ee360f642594e7b764e0859ac820b83f99
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2026-01-01 15:55:38 -0500

    wip

---
+++ .eslint-doc-generatorrc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/** @type {import('eslint-doc-generator').GenerateOptions} */
+const config = {
+    urlConfigs:
+        "https://github.com/platinumazure/eslint-plugin-qunit/blob/main/README.md#configurations",
+};
+
+module.exports = config;
